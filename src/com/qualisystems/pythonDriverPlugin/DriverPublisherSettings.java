231f4a4f6336484863859fe343f1eacba87c8682
mastern2k3 <nitzan.zada@gmail.com>
2016-02-24 16:28:49 +0200

    initial

---
+++ src/com/qualisystems/pythonDriverPlugin/DriverPublisherSettings.java
@@ -0,0 +1,44 @@
+package com.qualisystems.pythonDriverPlugin;
+
+import org.apache.commons.lang.ArrayUtils;
+
+import java.util.Properties;
+
+public class DriverPublisherSettings {
+
+    String serverRootAddress;
+    int port;
+    String driverUniqueName;
+    String username;
+    String password;
+    String domain;
+    String[] fileFilters;
+
+    private static final String[] DefaultFileFilters = new String[] { ".idea", "deployment", "deployment.xml" };
+
+    public static DriverPublisherSettings fromProperties(Properties deploymentProperties) throws IllegalArgumentException {
+
+        if (!deploymentProperties.containsKey("driverUniqueName"))
+            throw new IllegalArgumentException("Missing `driverUniqueName` key in project's deployment.xml");
+
+        if (!deploymentProperties.containsKey("serverRootAddress"))
+            throw new IllegalArgumentException("Missing `serverRootAddress` key in project's deployment.xml");
+
+        DriverPublisherSettings settings = new DriverPublisherSettings();
+
+        settings.serverRootAddress = deploymentProperties.getProperty("serverRootAddress");
+        settings.port = Integer.parseInt(deploymentProperties.getProperty("port", "8029"));
+        settings.driverUniqueName = deploymentProperties.getProperty("driverUniqueName");
+        settings.username = deploymentProperties.getProperty("username", "admin");
+        settings.password = deploymentProperties.getProperty("password", "admin");
+        settings.domain = deploymentProperties.getProperty("domain", "Global");
+
+        String fileFiltersValue = deploymentProperties.getProperty("fileFilters", "");
+
+        String[] extraFilters = fileFiltersValue.isEmpty() ? new String[0] : fileFiltersValue.split(";");
+
+        settings.fileFilters = (String[])ArrayUtils.addAll(DefaultFileFilters, extraFilters);
+
+        return settings;
+    }
+}
---
+++ src/com/qualisystems/pythonDriverPlugin/ZipHelper.java
@@ -0,0 +1,99 @@
+package com.qualisystems.pythonDriverPlugin;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+class ZipHelper {
+
+    private final String[] _filters;
+
+    public ZipHelper(String... filters) {
+
+        if (filters == null)
+            filters = new String[0];
+
+        _filters = filters;
+    }
+
+    public void zipDir(String dirName, String nameZipFile) throws IOException {
+
+        ZipOutputStream zip = null;
+        FileOutputStream fW = null;
+
+        Files.createDirectories(Paths.get(new File(nameZipFile).getParent()));
+
+        fW = new FileOutputStream(nameZipFile);
+        zip = new ZipOutputStream(fW);
+
+        initialAddFolderToZip(dirName, zip);
+
+        zip.close();
+        fW.close();
+    }
+
+    private void initialAddFolderToZip(String srcFolder, ZipOutputStream zip) throws IOException {
+
+        File folder = new File(srcFolder);
+
+        for (String fileName : folder.list()) {
+            addFileToZip("", srcFolder + "/" + fileName, zip, false);
+        }
+    }
+
+    private void addFolderToZip(String path, String srcFolder, ZipOutputStream zip) throws IOException {
+        File folder = new File(srcFolder);
+        if (folder.list().length == 0) {
+            addFileToZip(path , srcFolder, zip, true);
+        }
+        else {
+            for (String fileName : folder.list()) {
+                if (path.equals("")) {
+                    addFileToZip(folder.getName(), srcFolder + "/" + fileName, zip, false);
+                }
+                else {
+                    addFileToZip(path + "/" + folder.getName(), srcFolder + "/" + fileName, zip, false);
+                }
+            }
+        }
+    }
+
+    private void addFileToZip(String path, String srcFile, ZipOutputStream zip, boolean flag) throws IOException {
+
+        File folder = new File(srcFile);
+
+        if (flag) {
+
+            zip.putNextEntry(new ZipEntry(path + "/" +folder.getName() + "/"));
+
+        } else {
+
+            String nameInZip = (path.isEmpty() ? path : path + "/") + folder.getName();
+
+            for (String filter : _filters)
+                if (nameInZip.equalsIgnoreCase(filter))
+                    return;
+
+            if (folder.isDirectory()) {
+
+                addFolderToZip(path, srcFile, zip);
+
+            } else {
+
+                byte[] buf = new byte[1024];
+                int len;
+
+                FileInputStream in = new FileInputStream(srcFile);
+                zip.putNextEntry(new ZipEntry(nameInZip));
+
+                while ((len = in.read(buf)) > 0)
+                    zip.write(buf, 0, len);
+            }
+        }
+    }
+}
